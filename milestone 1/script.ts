ae73fcb414a715e168c38a2e7a47525e0f6ab970
muhammadtahir0 <161215017+muhammadtahir0@users.noreply.github.com>
2024-09-29 15:46:08 +0500

    Update script.ts

--- milestone 1/script.ts
+++ milestone 1/script.ts
@@ -1,10 +1,15 @@
-const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement
-const skills = document.getElementById('skills') as HTMLElement
+const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement;
+const skills = document.getElementById('Skills') as HTMLElement;
 
-toggleButton.addEventListener('click',()=> {
+// Initially show the skills section
+skills.style.display = 'block';
+
+toggleButton.addEventListener('click', () => {
     if (skills.style.display === 'none') {
-        skills.style.display = 'block'
+        skills.style.display = 'block';
+        toggleButton.innerHTML = '<b>Hide Skills</b>'; // Change button text
     } else {
-        skills.style.display === 'none'
+        skills.style.display = 'none';
+        toggleButton.innerHTML = '<b>Show Skills</b>'; // Change button text
     }
 });
