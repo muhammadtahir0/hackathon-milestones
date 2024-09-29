0c31e50ba29e87bbda43f315e5c959d92f7d9102
muhammadtahir0 <161215017+muhammadtahir0@users.noreply.github.com>
2024-09-29 15:49:02 +0500

    Update script.js

--- milestone 1/script.js
+++ milestone 1/script.js
@@ -1,10 +1,15 @@
-var toggleButtonOption = document.getElementById('toggle-skills');
-var skillsOption = document.getElementById('skills');
-toggleButtonOption.addEventListener('click', function () {
-    if (skillsOption.style.display === 'none') {
-        skillsOption.style.display = 'block';
-    }
-    else {
-        skillsOption.style.display === 'none';
+var toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement;
+var skills = document.getElementById('Skills') as HTMLElement;
+
+// Initially show the skills section
+skills.style.display = 'block';
+
+toggleButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+        toggleButton.innerHTML = '<b>Hide Skills</b>'; // Change button text
+    } else {
+        skills.style.display = 'none';
+        toggleButton.innerHTML = '<b>Show Skills</b>'; // Change button text
     }
 });
